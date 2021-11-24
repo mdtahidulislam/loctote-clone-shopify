c4142abcda8c16770e7f13aeebcf822e54b0eae5
tahidul1 <contact.tahidul@gmail.com>
2021-11-24 23:14:26 +0600

    added dynamic sticky header

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+// sticky header
+// const header = document.querySelector('#header');
+// const stickyValue = `{{ section.settings.sticky_header }}`;
+// window.onscroll = () => {
+//     if (window.pageYOffset > 140 && stickyValue == 'true') {
+//         header.classList.add('header--sticky', 'is-bg');
+//         console.log('yes');
+//     } else {
+//         header.classList.remove('header--sticky', 'is-bg');
+//     }
+// }
